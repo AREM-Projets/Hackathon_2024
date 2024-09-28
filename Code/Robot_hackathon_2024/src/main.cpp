3146a7e6a32cf0972f363c008cc6b383ee90ec10
Disperseur <disperseur.code@gmail.com>
2024-09-28 10:29:58 +0200

    update empruntes cms et modele 3d

--- Code/Robot_hackathon_2024/src/main.cpp
+++ Code/Robot_hackathon_2024/src/main.cpp
@@ -16,12 +16,12 @@ Marche arrière: 85 - 88
 #include <VL53L1X.h>
 
 #define TOF_XSDN D3
-#define MOTOR_D_BACKWARD_STOP 1565
-#define MOTOR_D_FORWARD_STOP 1465
+#define MOTOR_D_BACKWARD_STOP 1500//1565
+#define MOTOR_D_FORWARD_STOP 1500//1465
 
-#define MOTOR_G_BACKWARD_STOP 1475
-#define MOTOR_G_FORWARD_STOP 1575
-#define MOTOR_G_SPEED_RANGE 50
+#define MOTOR_G_BACKWARD_STOP 1500//1475
+#define MOTOR_G_FORWARD_STOP 1500//1575
+#define MOTOR_G_SPEED_RANGE 100
 
 #define MOTOR_STOPf_TO_STOPb_RANGE 100
 
@@ -46,9 +46,11 @@ void setup() {
   init_base(motorL, motorR);
 
 
- 
-  motorL.writeMicroseconds(MOTOR_G_FORWARD_STOP+MOTOR_G_SPEED_RANGE-7);
-  motorR.writeMicroseconds(MOTOR_D_FORWARD_STOP-MOTOR_G_SPEED_RANGE);
+  motorL.writeMicroseconds(2000);
+  motorR.writeMicroseconds(1000);
+
+  // motorL.writeMicroseconds(MOTOR_G_FORWARD_STOP+MOTOR_G_SPEED_RANGE-7);
+  // motorR.writeMicroseconds(MOTOR_D_FORWARD_STOP-MOTOR_G_SPEED_RANGE);
 
 }
 
