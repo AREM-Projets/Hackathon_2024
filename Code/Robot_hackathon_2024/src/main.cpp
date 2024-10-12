783bbda34bc28a2125449e6194d8eb037ce80533
Disperseur <disperseur.code@gmail.com>
2024-10-12 19:04:46 +0200

    test hard leds equipe et bouton

--- Code/Robot_hackathon_2024/src/main.cpp
+++ Code/Robot_hackathon_2024/src/main.cpp
@@ -14,12 +14,37 @@ Base robot;
 
 
 void setup() {
+  pinMode(D13, INPUT_PULLUP);
+  pinMode(D10, OUTPUT);
+  pinMode(D11, OUTPUT);
+
+  
+
+
+  if(digitalRead(D13))
+  {
+    
+    digitalWrite(D10, 1);
+    digitalWrite(D11, 0);
+  }
+  else
+  {
+    digitalWrite(D10, 0);
+    digitalWrite(D11, 1);
+  }
+  
+  
+  
 
   while (!Serial) {}
   Serial.begin(9600); // a mettre la ou il y a des Serial.print dans les classes
 
 
   robot.init();
+
+
+  
+
 
   //test de roulage en distance
   // robot.run(FORWARD);
